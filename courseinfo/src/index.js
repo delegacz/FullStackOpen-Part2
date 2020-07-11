4009198210c507901b7ea41440b133ac9390bb2b
Mateusz Delegacz <mat.delegacz@gmail.com>
2020-07-11 01:26:36 +0100

    2.4 competed

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -4,8 +4,12 @@ import ReactDOM from 'react-dom';
 const Course = ({course}) => {
   return (
     <div>
-      <Header course={course}/>
-      <Content course={course}/>
+      {course.map((course,i) => 
+        <>
+          <Header key={i} course={course}/>
+          <Content key={i} course={course}/>
+        </>
+    )}
     </div>
   )
   
@@ -37,44 +41,57 @@ const Part = ({part}) => {
 const Content = ({ course }) => {
   return (
     <div>
-      {course.parts.map((part) => <Part key={part.id} part={part} />
-    )}
+      {course.parts.map(part => <Part key={part.id} part={part} />)}
     <Total course={course}/>
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id:1 
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      },
-      {
-        name: 'Node',
-        exercises: 20,
-        id: 5
-      }
-    ]
-  }
+  const course = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
