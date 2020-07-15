a9b5188c1a505a60cfc813bbf61af29e26023ec8
Mateusz Delegacz <mat.delegacz@gmail.com>
2020-07-15 20:52:01 +0100

    2.13 completed

--- countries/src/App.js
+++ countries/src/App.js
@@ -4,6 +4,8 @@ import axios from 'axios'
 function App() {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState('');
+  const [clickedCountry, setClickedCountry] = useState('');
+  
   const getCountries = () => {
     console.log('fired')
     axios
@@ -18,14 +20,20 @@ function App() {
   }
   useEffect(getCountries,[])
   const handleSearchAction = (event) => {
-  setSearch(event.target.value)
+    setClickedCountry('')
+    setSearch(event.target.value)
+  }
+  
+  const handleShowEvent= (event) => {
+    setClickedCountry(event.target.value)
+    console.log(event.target.value)
   }
   return (
     <div className="App">
       <SearchField search={search} handleSearchAction={handleSearchAction}/>
     <div>
       <ul>
-        <FilterCountryList countries={countries} search={search}/>
+        <FilterCountryList countries={countries} search={search} handleShowEvent={handleShowEvent} clickedCountry={clickedCountry}/>
       </ul>
     </div>
     </div>
@@ -37,10 +45,16 @@ const FilterCountryList = (props) => {
     return (
       <p>Too many results to display</p>
     )
-  }  else {
+  }  else if(props.clickedCountry != '') { 
     return (
       <>
-      {props.countries.filter(country => country.name.toLowerCase().includes(props.search.toLowerCase())).map((country,i) => <DisplayCountry key={i} country={country} count={countOfReturnedCountries} countries={props.countries}/>)}
+      {props.countries.filter(country => country.name.match(props.clickedCountry)).map((country,i) => <DetailedCountryView key={i} country={country}/>)}
+      </>
+    )}
+  else {
+    return (
+      <>
+      {props.countries.filter(country => country.name.toLowerCase().includes(props.search.toLowerCase())).map((country,i) => <DisplayCountry key={i} country={country} count={countOfReturnedCountries} countries={props.countries} handleShowEvent={props.handleShowEvent}/>)}
       </>
     )
   }
@@ -50,20 +64,13 @@ const DisplayCountry = (props) => {
   console.log(props.country.flag)
   if(props.count===1) {
     return(<>
-      <h1>{props.country.name}</h1>
-      <p>Capital: {props.country.capital}</p>
-      <p>Population: {props.country.population}</p>
-      <h2>Languages</h2>
-      <ul>
-    {props.country.languages.map((language,i) => <li key={i}> {language.name}</li>)}
-      </ul>
-      <img src={props.country.flag} width="200px"/>
+      <DetailedCountryView country={props.country}/>
     </>)
   }
   else {
   return(
     <>
-      <li>{props.country.name}</li>
+      <li>{props.country.name} <button value={props.country.name} onClick={props.handleShowEvent}>Show</button></li>
     </>
   )}
 }
@@ -72,6 +79,21 @@ const SearchField = (props) => {
     <>
     <input value={props.search} onChange={props.handleSearchAction}/>
     <button>Search</button>
+    </>
+  )
+}
+
+const DetailedCountryView = (props) => {
+  return (
+    <>
+      <h1>{props.country.name}</h1>
+      <p>Capital: {props.country.capital}</p>
+      <p>Population: {props.country.population}</p>
+      <h2>Languages</h2>
+      <ul>
+        {props.country.languages.map((language,i) => <li key={i}> {language.name}</li>)}
+      </ul>
+      <img src={props.country.flag} width="200px"/>
     </>
   )
 }
